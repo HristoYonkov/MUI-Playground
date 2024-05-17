e59bda2f3da9efdd75c6e903bfdc57b621cd95f4
Hristo Yonkov <hristopturs@gmail.com>
2024-05-17 14:33:17 +0300

    add new dependencies

--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,16 @@ import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
 
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+const queryClient = new QueryClient({
+    defaultOptions: { queries: { staleTime: 60000, gcTime: 10 * (60 * 1000) } }, // Set options to all queries.
+});
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
-        <App />
+        <QueryClientProvider client={queryClient}>
+            <App />
+        </QueryClientProvider>
     </React.StrictMode>
 );
