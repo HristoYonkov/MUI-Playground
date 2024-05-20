5e899dbb89b91e495bf4fec480d1e80ad4c1ae94
Hristo Yonkov <hristopturs@gmail.com>
2024-05-20 18:16:01 +0300

    remove custom drawer from here

--- src/components/MainLayout/MainLayout.tsx
+++ src/components/MainLayout/MainLayout.tsx
@@ -3,26 +3,13 @@ import * as React from 'react';
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import CssBaseline from '@mui/material/CssBaseline';
-import Divider from '@mui/material/Divider';
 import Drawer from '@mui/material/Drawer';
 import IconButton from '@mui/material/IconButton';
-import List from '@mui/material/List';
-import ListItem from '@mui/material/ListItem';
-import ListItemButton from '@mui/material/ListItemButton';
-import ListItemIcon from '@mui/material/ListItemIcon';
-import ListItemText from '@mui/material/ListItemText';
-import PeopleAltOutlinedIcon from '@mui/icons-material/PeopleAltOutlined';
-import AccountTreeOutlinedIcon from '@mui/icons-material/AccountTreeOutlined';
-import LocalShippingOutlinedIcon from '@mui/icons-material/LocalShippingOutlined';
-import ManageAccountsOutlinedIcon from '@mui/icons-material/ManageAccountsOutlined';
-import Inventory2OutlinedIcon from '@mui/icons-material/Inventory2Outlined';
-import DifferenceOutlinedIcon from '@mui/icons-material/DifferenceOutlined';
 import MenuIcon from '@mui/icons-material/Menu';
 import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
 
-import logoBG from '../../images/logo_bg.png';
 import MainPageDropdown from '../MainPageDropdown/MainPageDropdown';
+import CustomDrawer from '../CustomDrawer/CustomDrawer';
 
 const drawerWidth = 304;
 
@@ -34,7 +21,7 @@ interface MainLayoutProps {
 export default function MainLayout({ children }: MainLayoutProps): React.ReactElement {
     const [mobileOpen, setMobileOpen] = React.useState(false);
     const [isClosing, setIsClosing] = React.useState(false);
-    
+
 
     const handleDrawerClose = () => {
         setIsClosing(true);
@@ -50,56 +37,6 @@ export default function MainLayout({ children }: MainLayoutProps): React.ReactEl
             setMobileOpen(!mobileOpen);
         }
     };
-
-    const hrefMap: { [key: number]: string } = {
-        0: '/users',
-        1: '/zones',
-        2: '/providers',
-        3: '/roles',
-        4: '/goods-types',
-        5: '/type-difference'
-    };
-
-    const drawer = (
-        <div>
-            <Toolbar component={'a'} href='/' sx={{ gap: '16px', padding: '0 16px 0 16px', height: '73.22px', textDecoration: 'none' }} disableGutters>
-                <img style={{ height: '40px', width: '40px', borderRadius: '50%' }} src={logoBG} alt='logo' />
-                <Typography sx={{ color: 'black' }} variant='h6' fontWeight={600}>
-                    Intercars Bulgaria
-                </Typography>
-            </Toolbar>
-            <Divider />
-            <List>
-                {['Потребители', 'Зони', 'Доставчици', 'Роли', 'Видове Стока', 'Тип Разлика'].map((text, index) => (
-                    <ListItem
-                        key={text}
-                        disablePadding
-                        sx={{
-                            '&:hover': {
-                                background: '#BB002133',
-                                color: '#C9022D',
-                                '& .MuiSvgIcon-root, & .MuiSvgIcon-fontSizeMedium, & .css-wg28wd-MuiSvgIcon-root': {
-                                    color: '#C9022D',
-                                },
-                            },
-                        }}
-                    >
-                        <ListItemButton LinkComponent={'a'} href={hrefMap[index] || ''}>
-                            <ListItemIcon>
-                                {index === 0 ? <PeopleAltOutlinedIcon sx={{ color: 'black' }} /> : null}
-                                {index === 1 ? <AccountTreeOutlinedIcon sx={{ color: 'black' }} /> : null}
-                                {index === 2 ? <LocalShippingOutlinedIcon sx={{ color: 'black' }} /> : null}
-                                {index === 3 ? <ManageAccountsOutlinedIcon sx={{ color: 'black' }} /> : null}
-                                {index === 4 ? <Inventory2OutlinedIcon sx={{ color: 'black' }} /> : null}
-                                {index === 5 ? <DifferenceOutlinedIcon sx={{ color: 'black' }} /> : null}
-                            </ListItemIcon>
-                            <ListItemText primary={text} />
-                        </ListItemButton>
-                    </ListItem>
-                ))}
-            </List>
-        </div>
-    );
 
     return (
         <Box sx={{ display: 'flex' }}>
@@ -150,7 +87,7 @@ export default function MainLayout({ children }: MainLayoutProps): React.ReactEl
                         },
                     }}
                 >
-                    {drawer}
+                    {<CustomDrawer />}
                 </Drawer>
                 <Drawer
                     variant='permanent'
@@ -163,7 +100,7 @@ export default function MainLayout({ children }: MainLayoutProps): React.ReactEl
                     }}
                     open
                 >
-                    {drawer}
+                    {<CustomDrawer />}
                 </Drawer>
             </Box>
             <Box
