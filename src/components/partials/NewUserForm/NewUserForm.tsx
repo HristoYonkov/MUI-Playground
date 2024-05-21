13a1303642ce3e4a3ea91aabee41b82296ee3efe
Hristo Yonkov <hristopturs@gmail.com>
2024-05-21 13:35:05 +0300

    update the form with basic placeholders

--- src/components/partials/NewUserForm/NewUserForm.tsx
+++ src/components/partials/NewUserForm/NewUserForm.tsx
@@ -1,6 +1,11 @@
 import * as React from 'react';
 import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
+import FormControl from '@mui/material/FormControl';
+import { InputLabel, MenuItem } from '@mui/material';
+import Select, { SelectChangeEvent } from '@mui/material/Select';
+import MyButton from '../MyButton/MyButton';
+import AddIcon from '@mui/icons-material/Add';
 
 interface NewUserFormProps {
     // title: string;
@@ -9,6 +14,11 @@ interface NewUserFormProps {
 }
 
 const NewUserForm: React.FC<NewUserFormProps> = ({}) => {
+    const [age, setAge] = React.useState('');
+
+  const handleChange = (event: SelectChangeEvent) => {
+    setAge(event.target.value as string);
+  };
     return (
         <Box
             component='form'
@@ -18,10 +28,29 @@ const NewUserForm: React.FC<NewUserFormProps> = ({}) => {
             noValidate
             autoComplete='off'
         >
+            <Box>
+                Title - Button
+            </Box>
             <TextField id='outlined-basic' label='Име' variant='outlined' />
             <TextField id='outlined-basic' label='Имейл' variant='outlined' />
             <TextField id='outlined-basic' label='Телефонен Номер' variant='outlined' />
-            <TextField id='outlined-basic' label='Роля' variant='outlined' />
+            <FormControl fullWidth>
+                <InputLabel id='demo-simple-select-label'>Age</InputLabel>
+                <Select
+                    labelId='demo-simple-select-label'
+                    id='demo-simple-select'
+                    value={age}
+                    label='Age'
+                    onChange={handleChange}
+                >
+                    <MenuItem value={10}>Ten</MenuItem>
+                    <MenuItem value={20}>Twenty</MenuItem>
+                    <MenuItem value={30}>Thirty</MenuItem>
+                </Select>
+            </FormControl>
+            <Box>
+                <MyButton title='Създаване' icon={<AddIcon />} buttonFn={() => {}} />
+            </Box>
         </Box>
     );
 };
