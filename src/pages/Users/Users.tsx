4944b31a677ac623c0f7cb9a998555e922f944e4
Hristo Yonkov <hristopturs@gmail.com>
2024-05-23 12:56:56 +0300

    Update Users.tsx

--- src/pages/Users/Users.tsx
+++ src/pages/Users/Users.tsx
@@ -6,6 +6,7 @@ import MainPageToolbar from '../../components/MainPageToolbar/MainPageToolbar';
 import MyModal from '../../components/MyModal/MyModal';
 import useMyModal from '../../hooks/useMyModal';
 import NewUserForm from '../../components/partials/NewUserForm/NewUserForm';
+import DataTable from '../../components/DataTable/DataTable';
 
 
 export default function Users(): React.ReactElement {
@@ -22,7 +23,8 @@ export default function Users(): React.ReactElement {
             <MyModal open={open} handleClose={handleClose}>
                 <NewUserForm handleClose={handleClose}></NewUserForm>
             </MyModal>
-            <Typography paragraph>Users..</Typography>
+            
+            <DataTable />
         </MainLayout>
     );
 }
