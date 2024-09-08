aa4584b8e9f22e55766830116e3a24052bcefb73
Hristo Yonkov <hristopturs@gmail.com>
2024-09-08 11:24:22 +0300

    Update NewDeliveryStep2Form.tsx

--- src/components/features/forms/newDeliveryForm/NewDeliveryStep2Form.tsx
+++ src/components/features/forms/newDeliveryForm/NewDeliveryStep2Form.tsx
@@ -4,18 +4,24 @@ import { Controller, UseFormReturn } from 'react-hook-form'
 import { useTranslation } from 'react-i18next'
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs'
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider'
-import { DatePicker } from '@mui/x-date-pickers/DatePicker'
 import { useNewDeliveryContext } from '@/hooks/useNewDeliveryContext'
 import dayjs from 'dayjs'
-
-const vendorName = ['Bosch', 'Valeo']
+import React from 'react'
+import { VendorDto } from '@/services/model'
+import useGetVendors from '@/hooks/services/vendors/useGetVendors'
+import { DateTimePicker } from '@mui/x-date-pickers/DateTimePicker'
 
 export default function NewDeliveryStep2Form({
   control,
+  setValue, //add setValue for dinamic control on values
   formState: { errors }
 }: UseFormReturn<NewDeliveryStep2FormData>) {
   const { t: translate } = useTranslation()
   const { formsData } = useNewDeliveryContext()
+  const vendors: VendorDto[] = useGetVendors()
+  const vendorsNames = vendors.map((vendor: VendorDto) => vendor.name!)
+
+  const [isVisibleVendorId, setIsVisibleVendorId] = React.useState(false)
 
   return (
     <>
@@ -27,9 +33,25 @@ export default function NewDeliveryStep2Form({
           <Autocomplete
             {...field}
             id="vendorName"
-            options={vendorName}
+            options={vendorsNames}
             value={field.value}
             onChange={(_, newValue) => {
+              if (newValue) {
+                setIsVisibleVendorId(true)
+                const selectedVendor = vendors.find(
+                  (vendor: VendorDto) => vendor.name! === newValue
+                )
+                if (selectedVendor) {
+                  setValue('vendorId', selectedVendor.id!.toString())
+                  setValue('vendorSystemNumber', selectedVendor.systemNumber!.toString())
+                } else {
+                  setValue('vendorId', '')
+                  setValue('vendorSystemNumber', '')
+                }
+              } else {
+                setIsVisibleVendorId(false)
+                setValue('vendorId', '')
+              }
               field.onChange(newValue)
             }}
             renderInput={(params) => (
@@ -44,23 +66,32 @@ export default function NewDeliveryStep2Form({
           />
         )}
       />
-      <Controller
-        name="vendorId"
-        control={control}
-        defaultValue={formsData.vendorId || ''}
-        render={({ field }) => (
-          <TextField
-            {...field}
-            label={translate('deliveries.newDelivery.labels.step2.vendorId')}
-            id="vendorId"
-            name="vendorId"
-            // readOnly
-            required
-            error={!!errors.vendorId}
-            helperText={errors.vendorId?.message ? translate(errors.vendorId.message) : ''}
-          />
-        )}
-      />
+
+      {(isVisibleVendorId || formsData.vendorId) && (
+        <Controller
+          name="vendorSystemNumber"
+          control={control}
+          defaultValue={formsData.vendorId || ''}
+          render={({ field }) => (
+            <TextField
+              {...field}
+              id="outlined-read-only-input"
+              label={translate('deliveries.newDelivery.labels.step2.vendorId')}
+              value={field.value}
+              InputProps={{
+                readOnly: true
+              }}
+              error={!!errors.vendorSystemNumber}
+              helperText={
+                errors.vendorSystemNumber?.message
+                  ? translate(errors.vendorSystemNumber.message)
+                  : ''
+              }
+            />
+          )}
+        />
+      )}
+
       <Controller
         name="truckNumber"
         control={control}
@@ -77,26 +108,30 @@ export default function NewDeliveryStep2Form({
           />
         )}
       />
+
       <Controller
-        name="deliveryDate"
+        name="deliveryTime"
         control={control}
-        defaultValue={formsData.deliveryDate || dayjs().toDate()}
+        defaultValue={formsData.deliveryTime || dayjs().toDate()}
         render={({ field }) => (
           <LocalizationProvider dateAdapter={AdapterDayjs}>
-            <DatePicker
+            <DateTimePicker
               {...field}
-              label={translate('deliveries.newDelivery.labels.step2.deliveryDate')}
+              label={translate('deliveries.newDelivery.labels.step2.deliveryTime')}
               value={dayjs(field.value)}
               onChange={(newValue) => {
-                field.onChange(newValue?.toDate())
+                field.onChange(newValue ? newValue.toDate() : null)
               }}
-              format="DD/MM/YYYY"
+              ampm={false} // remove AM/PM and show 24h form
+              slots={{
+                textField: TextField
+              }}
               slotProps={{
                 textField: {
                   required: true,
-                  error: !!errors.deliveryDate,
-                  helperText: errors.deliveryDate?.message
-                    ? translate(errors.deliveryDate.message)
+                  error: !!errors.deliveryTime,
+                  helperText: errors.deliveryTime?.message
+                    ? translate(errors.deliveryTime.message)
                     : ''
                 }
               }}
